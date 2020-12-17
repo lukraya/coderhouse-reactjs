eec8f2e671440911d47430dd146c79e72f1cbade
Lucía Krayacich <luciakrayacich@gmail.com>
2020-12-17 18:19:22 -0300

    ItemListContainer y CartWidget funcionales.

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import cartIcon from './media/cartIcon.jpg';
 
-const CartWidget = () => <a href="#">
-    <img src="./media/cartIcon.jpg" alt="Icono de carrito" width="30px" height="30px"></img>
+const CartWidget = () => <a href="#" id="cartWidget">
+    <img src={cartIcon} alt="Icono de carrito" width="30px" height="30px"></img>
 </a>;
 
 export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './NavBar.css';
 import CartWidget from './CartWidget'
 
-const NavBar = () => <nav>
-    <p id="brand">Coder Kiosco</p>
-    <div id="menuDerecha">
-        <ul id="menu">
-            <li className="categorias"><a href="#">Bebidas</a></li>
-            <li className="categorias"><a href="#">Galletitas</a></li>
-            <li className="categorias"><a href="#">Alfajores</a></li>
-        </ul>
-        <CartWidget id="cartWidget"/>
-    </div>
-</nav>;
+const NavBar = () => {
+    return (
+        <nav>
+            <p id="brand">Coder Kiosco</p>
+            <div id="menuDerecha">
+                <ul id="menu">
+                    <li className="categorias"><a href="#">Bebidas</a></li>
+                    <li className="categorias"><a href="#">Galletitas</a></li>
+                    <li className="categorias"><a href="#">Alfajores</a></li>
+                </ul>
+                <CartWidget/>
+            </div>
+        </nav>
+    )
+}
 
 export default NavBar;
