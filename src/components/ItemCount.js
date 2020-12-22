2a3185f348b39d8948e039c2aea1a918005fb8e5
Lucía Krayacich <luciakrayacich@gmail.com>
2020-12-22 17:57:48 -0300

    Clase 5: contador. Corrección 3.

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,43 +1,17 @@
 import React from 'react';
 import './ItemCount.css';
 
-const ItemCount = ({initial,stock}) => {
-    const [contador,setContador] = React.useState(initial);
-    const [stockB,setStockB] = React.useState(stock);
-    
-    const sumarContador = () => {
-        if (contador<stockB) {
-            setContador(contador+1)
-        }
-    }
-
-    const restarContador = () => {
-        if (contador>0) {
-            setContador(contador-1)
-        }
-    }
-    
-    const resetContador = () => {
-        setContador(0)
-    }
-
-    const onAdd = () => {
-        if (stockB>0) {
-            setStockB(stockB-contador)
-        };
-              
-    }
-       
+const ItemCount = ({initial,onAdd,onSubstract}) => {
+   
     return (
         <div id="itemCount">
             <p>Alfajor Shot triple</p>
             <div id="seleccionCant">
-                <button onClick={restarContador} className="controles">-</button>
-                <p>{contador}</p>
-                <button onClick={sumarContador} className="controles">+</button>
+                <button onClick={onSubstract} className="controles">-</button>
+                <p>{initial}</p>
+                <button onClick={onAdd} className="controles">+</button>
             </div>
-            <p>Stock: {stockB}</p>
-            <button onClick={()=>{onAdd();resetContador()}} id="agregar">Agregar al carrito</button>
+            <button id="agregar">Agregar al carrito</button>
         </div>
     )
 }
