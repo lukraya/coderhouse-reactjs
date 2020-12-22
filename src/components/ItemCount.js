1fa793cb4231e2f5b949ea03be42b4be936897f8
Lucía Krayacich <luciakrayacich@gmail.com>
2020-12-22 15:47:06 -0300

    Clase 5: contador. Corrección 2.

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './ItemCount.css';
 
-const ItemCount = ({initial,onAdd,onSubstract}) => {
-   
+const ItemCount = ({initial,stock}) => {
+    const [contador,setContador] = React.useState(initial);
+    const [stockB,setStockB] = React.useState(stock);
+    
+    const sumarContador = () => {
+        if (contador<stockB) {
+            setContador(contador+1)
+        }
+    }
+
+    const restarContador = () => {
+        if (contador>0) {
+            setContador(contador-1)
+        }
+    }
+    
+    const resetContador = () => {
+        setContador(0)
+    }
+
+    const onAdd = () => {
+        if (stockB>0) {
+            setStockB(stockB-contador)
+        };
+              
+    }
+       
     return (
         <div id="itemCount">
             <p>Alfajor Shot triple</p>
             <div id="seleccionCant">
-                <button onClick={onSubstract} className="controles">-</button>
-                <p>{initial}</p>
-                <button onClick={onAdd} className="controles">+</button>
+                <button onClick={restarContador} className="controles">-</button>
+                <p>{contador}</p>
+                <button onClick={sumarContador} className="controles">+</button>
             </div>
-            <button id="agregar">Agregar al carrito</button>
+            <p>Stock: {stockB}</p>
+            <button onClick={()=>{onAdd();resetContador()}} id="agregar">Agregar al carrito</button>
         </div>
     )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,26 +2,10 @@ import React from 'react';
 import ItemCount from './ItemCount';
 
 const ItemListContainer = ({greeting}) => {
-    const [stock,setStock] = React.useState(10);
-    const [initial,setInitial] = React.useState(1);
-
-    const onAdd = () => {
-        if (stock>1) {
-            setInitial(initial+1);
-            setStock(stock-1);
-        }                
-    }
-
-    const onSubstract = () => {
-        if (initial>1) {
-            setInitial(initial-1);
-            setStock(stock+1);
-        }      
-    }
     
     return (<>
         <h1>{greeting}</h1>
-        <ItemCount initial={initial} onAdd={onAdd} onSubstract={onSubstract}/>
+        <ItemCount initial={1} stock={10}/>
     </>
     )
 } 
