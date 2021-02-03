d02716b6bc82e54158ff8d71719a12cf069ad636
Lucía Krayacich <luciakrayacich@gmail.com>
2021-02-02 22:17:30 -0300

    Firebase/firestore integrado.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './components/styles.css';
 import NavBar from './components/NavBar';
@@ -6,8 +6,35 @@ import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Cart from './components/Cart';
 import CartProvider from './CartContext';
+import {firestore} from './firebase'
 
 const App = () => {
+
+  useEffect(()=>{
+    const itemsCollection = firestore.collection("items")
+    
+    /* const query = itemsCollection.get()
+    query
+    .then((resultado)=>{
+      resultado.docs.forEach((doc)=>{
+        console.log(doc.id)
+        console.log(doc.data())
+      })
+    })
+    .catch((err)=>{
+      console.log(err)
+    }) */
+
+    const query = itemsCollection.where("categoria", "==", "Bebidas")
+    query.get().then((res)=>{
+      res.docs.forEach((doc)=>{
+        console.log(doc.data())
+      })
+    }) 
+  })
+
+
+
   return (
     <CartProvider>
       <BrowserRouter>
