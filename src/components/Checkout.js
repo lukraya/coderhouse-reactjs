039381c203b4cbbc2d7440c62290437bfa9d31b8
Lucía Krayacich <luciakrayacich@gmail.com>
2021-02-08 03:21:28 -0300

    Desafío clase 13: Item Collection II.

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
+import { contexto } from '../CartContext'
+import Orden from './Orden'
 
 const Checkout = () => {
     const [orden, setOrden] = useState()
-    const [usuario, setUsuario] = useState({})
-    
+    const {cart, precioTotal} = useContext(contexto)
+
+    //Obtener y crear el objeto usuario
+    /* const [usuario, setUsuario] = useState() */
     const [name, setName] = useState()
     const [phone, setPhone] = useState()
     const [email, setEmail] = useState()
@@ -13,29 +17,45 @@ const Checkout = () => {
         const nuevoUsuario = {  nombre: name,
                                 telefono: phone,
                                 correo: email   }
-        setUsuario(nuevoUsuario)
+        /* setUsuario(nuevoUsuario) */
+        crearOrden(nuevoUsuario, setOrden)
     }
-    console.log(usuario)
+    const crearOrden = (usuario, callback)=>{
+        const newOrden = {  buyer: usuario,
+                            items: cart,
+                            total: precioTotal()    }
+        callback(newOrden)
+    }
+    //Será que al hacer setOrden, rerenderiza y los estados de name, phone y email quedan vacíos, 
+    //de ahí que en Orden.js diga que buyer is undefined?
+
+    /* console.log(usuario) */
+    console.log("Render Checkout")
+       
 
     return (
-        <div className="checkout">
-            <p>Por favor complete los datos</p>
-            <form onSubmit={(e)=>getUsuario(e)}>
-                <div className="formItem">
-                    <label htmlFor="nombre">Nombre: </label>
-                    <input onChange={(e)=>{setName(e.target.value)}} id="nombre" type="text" name="nombre" placeholder="José Perez" required></input>
-                </div>
-                <div className="formItem">
-                    <label htmlFor="celular">Celular: </label>
-                    <input onChange={(e)=>{setPhone(e.target.value)}} id="celular" type="tel" name="celular" placeholder="1133333333" required></input>
-                </div>
-                <div className="formItem">
-                    <label htmlFor="email">Email: </label>
-                    <input onChange={(e)=>{setEmail(e.target.value)}} id="email" type="email" name="email" placeholder="jperez@email.com" required></input>
-                </div>
-                <button type="submit">Terminar</button>
-            </form>
-        </div>
+        <>{!orden ?
+            (<div className="checkout">
+                <p>Por favor complete los datos</p>
+                <form onSubmit={(e)=>getUsuario(e)}>
+                    <div className="formItem">
+                        <label htmlFor="nombre">Nombre: </label>
+                        <input onChange={(e)=>{setName(e.target.value)}} id="nombre" type="text" name="nombre" placeholder="José Perez" required></input>
+                    </div>
+                    <div className="formItem">
+                        <label htmlFor="celular">Celular: </label>
+                        <input onChange={(e)=>{setPhone(e.target.value)}} id="celular" type="tel" name="celular" placeholder="1133333333" required></input>
+                    </div>
+                    <div className="formItem">
+                        <label htmlFor="email">Email: </label>
+                        <input onChange={(e)=>{setEmail(e.target.value)}} id="email" type="email" name="email" placeholder="jperez@email.com" required></input>
+                    </div>
+                    <button type="submit">Terminar</button>
+                </form>
+                
+            </div>)
+            : <Orden orden={orden}/>        
+        }</>
     )
 }
 
--- src/components/Orden.js
+++ src/components/Orden.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { firestore } from '../firebase'
 
-const Orden = () => {
-    return (
-        <>
-            <p>Orden de compra número:</p>
-            <p>Detalles:</p>
-            <p>Gracias por su compa!</p>
-        </>
-    )
+const Orden = (orden) => {
+    console.log(orden)
+    const [ordenId, setOrdenId] = useState()
+    
+    //Preparar nueva orden y crear doc en firestore
+    useEffect(()=>{
+        const orders = firestore.collection("orders")
+        if (!ordenId) {
+            orders.add(orden)
+            .then(({id})=>{
+                setOrdenId(id)
+                console.log(id)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+        }
+        
+    },[ordenId, orden])
+
+    console.log("Render Orden")
+
+    return (<>{ordenId ?
+                <>
+                    <p>Orden de compra número: {ordenId}</p>
+                    {/* <p>Detalles:</p> */}
+                    <p>Gracias por su compra!</p>
+                </>
+                :
+                <><p>Generando su orden</p></>
+    }</>)
 }
 
 export default Orden
